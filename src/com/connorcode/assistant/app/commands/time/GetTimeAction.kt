fcacda718698cfe8d44380dd8a142e4063c7d32d
Connor Slade <connor@connorcode.com>
2021-08-13 23:38:42 -0400

    ⏱️ Show system time too

--- src/com/connorcode/assistant/app/commands/time/GetTimeAction.kt
+++ src/com/connorcode/assistant/app/commands/time/GetTimeAction.kt
@@ -4,13 +4,16 @@ import com.connorcode.assistant.Assistant
 import com.connorcode.assistant.Common
 import com.connorcode.assistant.app.Action
 import com.connorcode.assistant.app.Response
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class GetTimeAction : Action() {
     private val keywords = arrayOf("what", "is", "the", "time", "current", "epoch")
     private val weight = intArrayOf(1, 1, 1, 5, 4, 6)
     override fun doCommand(command: String) {
-        val time = System.currentTimeMillis()
-        Assistant.getInstance().displayItem(Response("Current Epoch Time: " + time / 1000))
+        val epoch = System.currentTimeMillis()
+        val normal = LocalDateTime.now().format(DateTimeFormatter.ofPattern("HH:mm:ss"))
+        Assistant.getInstance().displayItem(Response("Current Epoch Time:  ${epoch / 1000}\nCurrent System Time: $normal"))
     }
 
     override fun getLikelihood(command: String): Double {
